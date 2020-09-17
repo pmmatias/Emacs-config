1ae8fcb0db893d9d7a15eceee3aa4cfa700712d8
Pedro Matias <pedro@pmmatias.com>
2020-09-17 21:23:08 +0100

    Add defun, new-tmp-buffer, to create temporary buffers

--- defuns/buffer-defuns.el
+++ defuns/buffer-defuns.el
@@ -4,3 +4,10 @@
   (mapcar (lambda (x) (kill-buffer x))
           (buffer-list))
   (delete-other-windows))
+
+
+(defun new-tmp-buffer ()
+  "Create a new temporary buffer
+When killed, it won't remind you to save it"
+  (interactive)
+  (switch-to-buffer (generate-new-buffer "tmp")))
