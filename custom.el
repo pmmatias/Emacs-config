2954d294e15b4d4aad25399c6bfcfd175e7b6085
Pedro Matias <pmmatias10@gmail.com>
2023-03-06 16:23:39 +0000

    Enable the execution of org babel code blocks

--- custom.el
+++ custom.el
@@ -1,2 +1,5 @@
 ;; (setq org-agenda-files (list "~/agenda"))
 ;; (setq org-roam-directory (file-truename "~/agenda/org-roam"))
+
+;; Setup Org-roam to automatically sync. file changes to its DB
+;; (org-roam-db-autosync-mode)
--- modules/pemacs-org.el
+++ modules/pemacs-org.el
@@ -1,11 +1,19 @@
+;; Load org Babel languages to enable their execution at code blocks
+(org-babel-do-load-languages
+ 'org-babel-load-languages
+ '((C . t)
+   (java . t)
+   (python . t)
+   (shell . t)))
+
+;; Don't ask for confirmation before executing code blocks
+(setq org-confirm-babel-evaluate nil)
+
 ;; Enable org-bullets (show org-mode bullets as UTF-8 characters)
 (require 'org-bullets)
 (add-hook 'org-mode-hook 'org-bullets-mode)
 
 ;; Enable orgbox (allows scheduling for today/next week/etc., instead of a specific date)
 (require 'orgbox)
-
-;; Setup Org-roam to automatically sync. file changes to its DB
-;; (org-roam-db-autosync-mode)
 
 (provide 'pemacs-org)
