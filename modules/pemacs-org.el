a376fc776fab070c251e300b2a7a1a71f7856d54
Pedro Matias <pmmatias10@gmail.com>
2024-03-03 19:37:06 +0000

    Change Org Mode's default settings for indentation and folding

--- modules/pemacs-org.el
+++ modules/pemacs-org.el
@@ -1,3 +1,9 @@
+;; Indent the content accross different levels
+(setq org-startup-indented t)
+
+;; Open documents folded by default
+(setq org-startup-folded t)
+
 ;; Load org Babel languages to enable their execution at code blocks
 (org-babel-do-load-languages
  'org-babel-load-languages
